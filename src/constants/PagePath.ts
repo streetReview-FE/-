c91254e2bf4ab0981fadd40214f1c07872a66983
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-01-27 23:16:17 +0900

    Feat: 경로 모음 생성

---
+++ src/constants/PagePath.ts
@@ -0,0 +1,10 @@
+const PagePath = {
+    // mypage_path
+    MypageRivew: "/mypage/review", //내가 작성한 리뷰
+    MypageMycomment: "/mypage/mycomment", // 내 댓글
+    MypageLikecomment: "/mypage/likeComment", // 좋아요 누른 댓글
+    MypageUserSecession: "/mypage/secession" // 계정 삭제
+  };
+  
+  export default PagePath;
+  
