f4bef1db0bbf36be9372fa11008d9e658b29ff9e
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-01-27 23:14:12 +0900

    Feat: Color컴포넌트 orange, gray, container 백그라운드 배경색 추가

--- src/Styles/Colors.ts
+++ src/Styles/Colors.ts
@@ -4,6 +4,9 @@ const Colors = {
   mainfontColor: "rgb(255,154,131)",
   loveiconColor: "rgb(255,54,8)",
   logoColor: "rgb(39,9,73)",
+  backgroundColor: "rgba(97, 97, 97, 0.60)",
+  fontSelectColor: "rgba(239, 125, 22, 1)",
+  fontBasicGrayColor:"rgba(156, 156, 156, 1)"
 };
 
 export default Colors;
