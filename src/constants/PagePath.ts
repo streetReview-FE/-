3a4e74ccf9d1986623c533c4f2a4e5cc898ab378
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-02-04 17:24:23 +0900

    Docs: 페이지 주소 추가

--- src/constants/PagePath.ts
+++ src/constants/PagePath.ts
@@ -1,5 +1,12 @@
 const PagePath = {
-    // mypage_path
+  Home: "/",
+  // 거리요청
+  Requestpage: "/request",
+
+  // 리뷰작성페이지
+  PostReview: "/postview",
+  // mypage_path
+    Mypage: "/mypage",
     MypageRivew: "/mypage/review", //내가 작성한 리뷰
     MypageMycomment: "/mypage/mycomment", // 내 댓글
     MypageLikecomment: "/mypage/likeComment", // 좋아요 누른 댓글
